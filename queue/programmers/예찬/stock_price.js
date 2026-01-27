bbced273f62b1cd621267324e0e952df2a2f5165
YeChan Kim <az20058@naver.com>
2026-01-27 23:53:10 +0900

    [프로그래머스] 스택/주식가격 김예찬

---
+++ queue/programmers/예찬/stock_price.js
@@ -0,0 +1,19 @@
+function solution(prices) {
+  const answer = Array(prices.length).fill(0);
+  const stack = [];
+
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+      const prev = stack.pop();
+      answer[prev] = i - prev;
+    }
+    stack.push(i);
+  }
+
+  while (stack.length > 0) {
+    const prev = stack.pop();
+    answer[prev] = prices.length - 1 - prev;
+  }
+
+  return answer;
+}
