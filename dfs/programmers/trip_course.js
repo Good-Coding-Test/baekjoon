b3e294f3905851b78ca03bad3eaf6760d786e7dc
YeChan Kim <az20058@naver.com>
2026-02-08 18:54:12 +0900

    [프로그래머스] 여행경로/오일러경로/백트래킹/dfs 김예찬

--- dfs/programmers/trip_course.js
+++ dfs/programmers/trip_course.js
@@ -24,3 +24,36 @@ function solution(tickets) {
 
   return answer.reverse();
 }
+
+function solution(tickets) {
+  var answer = [];
+
+  let visited = Array(tickets.length).fill(false);
+
+  tickets.sort();
+
+  function dfs(cur, arr) {
+    if (arr.length === tickets.length + 1) {
+      answer = arr;
+      return true;
+    }
+
+    for (let i = 0; i < tickets.length; i++) {
+      let [start, end] = tickets[i];
+
+      if (start === cur && !visited[i]) {
+        visited[i] = true;
+
+        if (dfs(end, [...arr, end])) return true;
+
+        visited[i] = false;
+      }
+    }
+
+    return false;
+  }
+
+  dfs("ICN", ["ICN"]);
+
+  return answer;
+}
