9cc8fcc772544bdbe8c79c89fe5f5585d812de41
YeChan <az20058@naver.com>
2026-04-23 10:57:42 +0900

    [백준] 스택/오큰수 GOLD4

---
+++ stack/예찬/17298.js
@@ -0,0 +1,20 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input[0];
+const arr = input[1].split(" ").map(Number);
+let answer = Array(N).fill(-1);
+const stack = [];
+
+for (let i = 0; i < N; i++) {
+  while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
+    answer[stack.pop()] = arr[i];
+  }
+
+  stack.push(i);
+}
+
+console.log(answer.join(" "));
